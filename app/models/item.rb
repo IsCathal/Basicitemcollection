b7f9f8f74de4a088adb5f0c08823c0a29b73bf71
IsCathal <mccabemcathal@gmail.com>
2024-06-28 06:56:50 +0100

    It Workded

---
+++ app/models/item.rb
@@ -0,0 +1,9 @@
+class Item < ApplicationRecord
+
+  validates :name, presence: true,
+                  length: {minimum: 3, maximum: 50}
+
+  validates :description, presence: true, 
+                          length: {minimum: 10, maximum: 300} 
+                
+end
